640c9bfcf1e0d081ef0ad8a7ead13ba78b4636a1
CaptainTheAbysswalker <shuric03@gmail.com>
2021-02-11 19:33:40 +0300

    create text loading function

---
+++ src/functions/getText.js
@@ -0,0 +1,12 @@
+import { GETTEXT, LOADED, REPLACE } from "../redux/types";
+
+export const getText = async (dispatch) => {
+  const req = fetch(
+    "https://baconipsum.com/api/?type=meat-and-filler&paras=2"
+  ).then((res) => res.json());
+  req.then((data) =>
+    dispatch({ type: GETTEXT, payload: data.join("").split("") })
+  );
+
+  dispatch({ type: LOADED });
+};
