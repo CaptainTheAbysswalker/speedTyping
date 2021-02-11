1e7cccdd74e1c7d10673656169861f490b737d1b
CaptainTheAbysswalker <shuric03@gmail.com>
2021-02-11 19:34:38 +0300

    added compnents to app, load text to store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
+import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
+import Loader from "./components/Loader/Loader";
+import Text from "./components/Text/Text";
+import { getText } from "./functions/getText";
+import { REPLACE } from "./redux/types";
 
 function App() {
-  return <div className="App"></div>;
+  const isLoaded = useSelector((state) => state.stateReducer.isLoaded);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    getText(dispatch);
+    document.addEventListener("keypress", (event) => {
+      dispatch({ type: REPLACE });
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoaded && <Text />}
+      {!isLoaded && <Loader />}
+    </div>
+  );
 }
 
 export default App;
