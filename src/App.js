71ca34cce37c97fad1c5d470237d6dafb4880b66
CaptainTheAbysswalker <shuric03@gmail.com>
2021-02-12 20:09:43 +0300

    added result component, added timer id

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Loader from "./components/Loader/Loader";
+import Result from "./components/Result/Result";
 import Stats from "./components/Stats/Stats";
 import Text from "./components/Text/Text";
 import { getText } from "./functions/getText";
@@ -10,6 +11,10 @@ import { INCREASETIMER, REPLACE, RUNTIMER } from "./redux/types";
 function App() {
   const isLoaded = useSelector((state) => state.stateReducer.isLoaded);
   const isRun = useSelector((state) => state.stateReducer.isRun);
+  const timerId = useRef(0);
+  const textLength = useSelector((state) => state.textReducer.text.length);
+  const currentPos = useSelector((state) => state.textReducer.currentPos);
+
   const dispatch = useDispatch();
 
   const checkSymbol = (key) => {
@@ -28,7 +33,7 @@ function App() {
         if (!isRun) {
           dispatch({ type: RUNTIMER });
           let counter = 0;
-          setInterval(() => {
+          timerId.current = setInterval(() => {
             counter++;
             dispatch({ type: INCREASETIMER, payload: counter });
           }, 1000);
@@ -48,6 +53,7 @@ function App() {
   return (
     <div className="App">
       <Stats />
+      {textLength === currentPos && <Result id={timerId} />}
       {isLoaded && <Text />}
       {!isLoaded && <Loader />}
     </div>
