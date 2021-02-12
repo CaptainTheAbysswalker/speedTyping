83ef5713b49fc064f4dd202a8df585d9a1811646
CaptainTheAbysswalker <shuric03@gmail.com>
2021-02-12 18:17:12 +0300

    create Speed calculate component

---
+++ src/components/Speed/Speed.js
@@ -0,0 +1,25 @@
+import { React } from "react";
+import { useSelector } from "react-redux";
+
+import "./speed.css";
+
+const Speed = () => {
+  const time = useSelector((state) => state.textReducer.timer);
+  const currentPos = useSelector((state) => state.textReducer.currentPos);
+
+  const mathSpeed = () => {
+    if (currentPos === 0) {
+      return `0 s/m`;
+    } else {
+      return `${Math.round((currentPos / time) * 60)} s/m`;
+    }
+  };
+
+  return (
+    <div className="speedCounter">
+      <div>{mathSpeed()}</div>
+    </div>
+  );
+};
+
+export default Speed;
