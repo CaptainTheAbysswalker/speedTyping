7e7af941e3d080c3644656a04aca0dc3db436a18
CaptainTheAbysswalker <shuric03@gmail.com>
2021-02-12 18:18:53 +0300

    create timer events

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import "./App.css";
 import Loader from "./components/Loader/Loader";
+import Stats from "./components/Stats/Stats";
 import Text from "./components/Text/Text";
 import { getText } from "./functions/getText";
-import { REPLACE } from "./redux/types";
+import { INCREASETIMER, REPLACE, RUNTIMER } from "./redux/types";
 
 function App() {
   const isLoaded = useSelector((state) => state.stateReducer.isLoaded);
-
+  const isRun = useSelector((state) => state.stateReducer.isRun);
   const dispatch = useDispatch();
 
   const checkSymbol = (key) => {
+    dispatch({ type: RUNTIMER });
     dispatch({ type: REPLACE, payload: key });
   };
 
@@ -20,6 +22,20 @@ function App() {
     document.addEventListener("keypress", (event) => {
       checkSymbol(event.key);
     });
+    document.addEventListener(
+      "keypress",
+      () => {
+        if (!isRun) {
+          dispatch({ type: RUNTIMER });
+          let counter = 0;
+          setInterval(() => {
+            counter++;
+            dispatch({ type: INCREASETIMER, payload: counter });
+          }, 1000);
+        }
+      },
+      { once: true }
+    );
   }, []);
 
   useEffect(() => {
@@ -31,6 +47,7 @@ function App() {
   }, []);
   return (
     <div className="App">
+      <Stats />
       {isLoaded && <Text />}
       {!isLoaded && <Loader />}
     </div>
